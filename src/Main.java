69a4565f159d07e7f64ad0051482fba588d57a0c
marco <marco@DESKTOP-UI5SKR1>
2019-03-11 19:08:14 -0500

    Prueba #2 comentario

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Será que esto funcionó por fin ?");
-        //comentario prueba
+        //comentario prueba2    
     }
     
 }
